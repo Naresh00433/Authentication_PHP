90a913d26eec5ce4689adc4e7429a7e9132d02c2
Naresh <98719480+Naresh00433@users.noreply.github.com>
2025-03-25 12:20:19 +0530

    LoginFunctioning

--- partials/_nav.php
+++ partials/_nav.php
@@ -1,4 +1,14 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+<?php
+
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  $loggedin= true;
+}
+else{
+  $loggedin = false;
+}
+
+echo '<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">PHP Authentication</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,22 +18,30 @@
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/login_page/welcome.php">Home</a>
-        </li>
-        <li class="nav-item">
+        </li>';
+
+        if(!$loggedin){
+        echo '<li class="nav-item">
           <a class="nav-link" href="/login_page/login.php">Login</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/login_page/signup.php">Signup</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Logout</a>
-        </li>
+        </li>';
+        }
+
+        if($loggedin){
+        echo '<li class="nav-item">
+          <a class="nav-link" href="/login_page/logout.php">Logout</a>
+        </li>';  
         
-      </ul>
+        }
+      echo '</ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
-</nav>
+</nav>' ;
+
+?>
